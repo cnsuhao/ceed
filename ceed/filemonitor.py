5018e2d23412d0badef3ec9ba3cb926ef1bf7e79
Martin Preisler <preisler.m@gmail.com>
2011-12-29 17:16:46 +0100

    Slight changes to filemonitor to make it more python and make the style match CEED's

--- ceed/filemonitor.py
+++ ceed/filemonitor.py
@@ -1,39 +1,63 @@
+################################################################################
+#   CEED - A unified CEGUI editor
+#   Copyright (C) 2011 Martin Preisler <preisler.m@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 from PySide import QtGui, QtCore
 from PySide.QtGui import QMessageBox
 
-class filemonitor():
-    def __init__(self, parent, fileName, method):
+class FileMonitor(object):
+    def __init__(self, parent, fileName, reloadFileCallback):
         self.isActiveWindow = False
         self.fileChanged = False
         self.fileName = fileName
-        self.methodOnYes = method
+        self.reloadFileCallback = reloadFileCallback
         self.displayingAlert = False
+        
         self.monitor = QtCore.QFileSystemWatcher(parent)
-        QtCore.QObject.connect(self.monitor,QtCore.SIGNAL("fileChanged(const QString&)"), self.__activate__)
+        self.monitor.fileChanged.connect(self.slot_fileChanged)
         self.monitor.addPath(fileName)
-    def __activate__(self):
+        
+    def slot_fileChanged(self):
         self.fileChanged = True
         if self.isActiveWindow:
             self.displayReloadMessage()
-    def toggle(self, switch):
-        self.isActiveWindow = switch
-        if switch and self.fileChanged:
+            
+    def toggle(self, active):
+        self.isActiveWindow = active
+        if active and self.fileChanged:
             self.displayReloadMessage()
+            
     def changeToNewFile(self, newFile):
         self.monitor.removePath(self.fileName)
         self.monitor.addPath(newFile)
         self.fileName = newFile
+        
     def displayReloadMessage(self):
         if not self.displayingAlert:
             self.displayingAlert = True
             msgBox = QMessageBox()
-            msgBox.setText("An outside program modified the file.")
-            msgBox.setInformativeText("Reload the file?")
+            msgBox.setText("File has been modified externally!")
+            msgBox.setInformativeText("Should CEED reload the file? If you click Yes and then save, the contents that were externally changed will be overwritten!")
             msgBox.setStandardButtons(QMessageBox.No | QMessageBox.Yes)
             msgBox.setDefaultButton(QMessageBox.Yes)
             ret = msgBox.exec_()
             self.fileChanged = False
+            
             if ret == QMessageBox.Yes:
-                self.methodOnYes()
+                self.reloadFileCallback()
+                
             self.displayingAlert = False
-
